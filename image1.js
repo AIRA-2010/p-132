42f81d8b5946b55cbcbbad06300a5b4ddcf409df
AIRA-2010 <95174567+AIRA-2010@users.noreply.github.com>
2023-05-20 13:11:53 +0530

    Add files via upload

---
+++ image1.js
@@ -0,0 +1,27 @@
+img = "";
+status = "";
+object = [];
+function preload(){
+img = loadImage("Master_Bedroom.webp");
+}
+function setup(){
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting objects";
+}
+function modelLoaded(){
+    console.log("model is initialized");
+    status = true;
+    objectDetector.detect(img,gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+        console.log(results);
+        object = results;
+}
+function draw(){
+    image(img,0,0,640,420);
+}
